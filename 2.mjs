7c361a46ac5c826f1aeeaa93a5670f0d1d993c95
Florian Klampfer <mail@qwtel.com>
2019-02-07 16:08:47 +0700

    Update README.md

--- 2.mjs
+++ 2.mjs
@@ -7,6 +7,23 @@ import { read, pipe, some, sum, map, filter, frequencies, zip, combinations, fin
 
     const ids = input.trim().split('\n');
 
+    const [twos, threes] = pipe(
+        ids,
+        map(id => frequencies(id)),
+        map(fqs => [
+            pipe(fqs.values(), some(x => x === 2)),
+            pipe(fqs.values(), some(x => x === 3)),
+        ]),
+        unzip2(),
+        map(xs => pipe(xs,
+            map(x => x ? 1 : 0),
+            sum(),
+        )),
+    );
+
+    const checksum = twos * threes;
+    console.log(checksum);
+
     // const twos = [], threes = [];
     // for (const id of ids) {
     //     const freq = frequencies(id);
@@ -27,37 +44,8 @@ import { read, pipe, some, sum, map, filter, frequencies, zip, combinations, fin
     // }
     // console.log(twos.length * threes.length);
 
-    const [twos, threes] = pipe(
-        ids,
-        map(id => frequencies(id)),
-        map(fqs => [
-            pipe(fqs.values(), some(x => x === 2)),
-            pipe(fqs.values(), some(x => x === 3)),
-        ]),
-        unzip2(),
-        map(xs => pipe(xs,
-            map(x => x ? 1 : 0),
-            sum(0),
-        )),
-    );
-
-    const checksum = twos * threes;
-    console.log(checksum);
-
     // 2:
     const maxlen = ids[0].length;
-
-    // for (const [id1, id2] of combinations(ids, ids)) {
-    //     const res = [...pipe(
-    //         zip(id1, id2), 
-    //         filter(([l1, l2]) => l1 === l2), 
-    //         map(([l]) => l)
-    //     )]; 
-    //     if (res.length === maxlen - 1) {
-    //         console.log(res.join(''))
-    //         break;
-    //     }
-    // }
 
     const id = pipe(
         combinations(ids, ids),
@@ -71,5 +59,16 @@ import { read, pipe, some, sum, map, filter, frequencies, zip, combinations, fin
     );
 
     console.log(id);
+
+    // for (const [id1, id2] of combinations(ids, ids)) {
+    //     const res = [];
+    //     for (const [c1, c2] of zip(id1, id2)) {
+    //         if (c1 === c2) res.push(c1);
+    //     }
+    //     if (res.length === maxlen - 1) {
+    //         console.log(res.join(''))
+    //         break;
+    //     }
+    // }
 
 })();
--- README.md
+++ README.md
@@ -4,8 +4,48 @@ JavaScript solutions for [Advent of Code 2018][AoC] using the latest ECMAScript 
 [AoC]: https://adventofcode.com/2018
 
 ## Run
-Input via `stdin`, e.g. 
+Requires node 11+. Input via `stdin`, e.g. 
 
     cat 1.txt | ./1.mjs
 
 `.mjs` files are exectutable to avoid having to type `node --experimental-modules` over and over again.
+
+## Not your granpa's JS
+With generators, complex computations can be represented using functional building blocks. For example, finding the first duplicate in an (infinite) series of additions can be expressed as:
+
+~~~js
+console.log(pipe(
+    cycle(input),
+    reductions((a, b) => a + b, 0),
+    reductions(
+        (seen, freq) => seen.has(freq)
+            ? { reduced: freq }
+            : seen.add(freq),
+        new Set()
+    ),
+    find(({ reduced }) => reduced),
+).reduced);
+~~~
+
+Thanks to the pull-based nature of iterators, we can start with an infinite series (`cycle(input)`) but stop producing new items as soon as we `find` a result.
+
+In other words, the above solution does not run any more iterations as a more verbose implementation using an infinite loop.
+
+~~~js
+const seen = new Set();
+let freq = 0;
+let i = 0;
+while (true) {
+    freq += input[i];
+    if (seen.has(freq)) {
+        console.log(freq);
+        break;
+    }
+    seen.add(freq);
+    i = (i + 1) % input.length;
+}
+~~~
+
+Why is the first solution "better"? It's really a matter of taste, but personally I prefer the fact that each idea is expressed separately. I.e. the cycling of the input is one line instead of explicitly incrementing and wrapping and index, as is the lookup in the set, etc...
+
+Obviously the first solutions requires a variety of helper functions such as `pipe`, `reductions`, `find`, etc. However, as a look at [`util.mjs`](./util.mjs) will show, they are suprisingly easy to implement with the help of generator functions.
